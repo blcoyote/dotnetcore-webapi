4d173e4cf0810760892a3724b3b7c4fc9397d7e1
Brian Leach Christensen <blc@nuuday.dk>
2021-08-20 08:47:22 +0200

    database crud

--- Testapi/IRepository/IGenericRepository.cs
+++ Testapi/IRepository/IGenericRepository.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
-namespace Testapi.IRepository
+namespace HotelListing.IRepository
 {
 	public interface IGenericRepository<T> where T : class
 	{
